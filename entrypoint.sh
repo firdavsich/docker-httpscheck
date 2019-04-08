593b962927e36927c41b8b4da690bf32035a95ce
firdavsich <firdavs.murodov@gmail.com>
2019-04-08 14:56:11 +0500

    report only fails

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,5 @@
 
 while sleep ${INTERVAL}
 do
-if curl -s https://${HTTPS_HOST} > /dev/null; then
-    echo `date +"%Y-%m-%d %H:%M"` "HTTPS check Success"
-else
-    echo `date +"%Y-%m-%d %H:%M"` "HTTPS check Failure"
-fi
+curl -sH 'Cache-Control: no-cache' https://${HTTPS_HOST} > /dev/null || echo `date +"%Y-%m-%d %H:%M"` "HTTPS check Failure"
 done
